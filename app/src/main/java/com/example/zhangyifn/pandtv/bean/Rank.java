3d908f13f6ff57429ba9561a4d799283eaba5991
zhangyifn <584970275@qq.com>
2018-05-07 17:34:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhangyifn/pandtv/bean/Rank.java
@@ -0,0 +1,10 @@
+package com.example.zhangyifn.pandtv.bean;
+
+/**
+ * @author Jenly <a href="mailto:jenly1314@gmail.com">Jenly</a>
+ * @since 2017/3/7
+ */
+
+public class Rank {
+
+}
---
+++ app/src/main/java/com/example/zhangyifn/pandtv/mvp/APIRetrofitService.java
@@ -0,0 +1,9 @@
+package com.example.zhangyifn.pandtv.mvp;
+
+/**
+ * Created by zhangyifn on 2018/5/7.
+ */
+
+public class APIRetrofitService {
+
+}
